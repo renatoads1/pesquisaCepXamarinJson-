20287b1154f90daefb3280242212f0bf7823228a
Renato de Azevedo Lacerda <renatolacerda@matur.com.br>
2020-11-23 14:21:11 -0300

    cep

---
+++ ConsultarCepApp/ConsultarCepApp/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ConsultarCepApp.Servico.Modelo;
+using ConsultarCepApp.Servico;
+
+namespace ConsultarCepApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Botao.Clicked += btnOnclic;
+
+        }
+
+        private void btnOnclic(object sender, EventArgs args) {
+            string cep = Cep.Text.Trim();
+            if (cepValido(cep))
+            {
+                try
+                {
+                    if (cep != null)
+                    {
+                        Endereco end = ViaCepService.BuscaEnderecoViacep(cep);
+                        Resultado.Text = "Rua" + end.logradouro;
+
+                    }
+                    else {
+                        DisplayAlert("ERRO CRITICO","Cep Não existe -> "+Cep.Text, "ok");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRITICO", e.ToString() , "ok");
+                }
+                
+            }
+            else { 
+                
+            } 
+            
+        }
+
+        private bool cepValido(string cep)
+        {
+            bool valido = true;
+            if (cep.Length != 8) {
+                valido = false;
+                DisplayAlert("ERRO","O Cep deve ter 8 caracteres!!!","ok");
+            }
+            int cepNovo = 0;
+            if (!int.TryParse(cep,out cepNovo)) {
+                valido = false;
+                DisplayAlert("ERRO", "O Cep deve ter Apenas Números", "ok");
+            }
+
+            return valido;
+        }
+
+
+    }
+}
---
+++ ConsultarCepApp/ConsultarCepApp/Servico/Modelo/ViaCepService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ConsultarCepApp.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace ConsultarCepApp.Servico.Modelo
+{
+    public class ViaCepService
+    {
+        private static string EnderecoUrl = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscaEnderecoViacep(string cep) {
+            string NovoEnderecoUrl = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+
+
+            string retorno = wc.DownloadString(NovoEnderecoUrl);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(retorno);
+            if (end.cep == null)
+            {
+                return null;
+            }
+            else {
+                return end;
+            }
+            
+        }
+
+        
+        
+
+    }
+}
